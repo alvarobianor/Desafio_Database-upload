bad6ccbeeba2c50bc51d9918ce9a404c3e023c2e
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-04-26 15:42:45 -0300

    falta so upar

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,10 +8,48 @@ interface Balance {
   total: number;
 }
 
+// refac after
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
     // TODO
+    const incomes = await this.find({ where: { type: 'income' } });
+
+    const income = !incomes[0]
+      ? 0
+      : incomes
+          .map(e => {
+            if (!e) {
+              return 0;
+            }
+            return e.value;
+          })
+          .reduce((total, acumulator) => total + acumulator);
+
+    const outcomes = await this.find({ where: { type: 'outcome' } });
+
+    const outcome = !outcomes[0]
+      ? 0
+      : outcomes
+          .map(e => {
+            if (!e) {
+              return 0;
+            }
+            return e.value;
+          })
+          .reduce((total, acumulator) => total + acumulator);
+
+    // const outcomes = (await this.find({ where: { type: 'outcome' } })) || [0];
+    // const outcome = outcomes
+    //   .map(e => {
+    //     if (!e) {
+    //       return 0;
+    //     }
+    //     return e.value;
+    //   })
+    //   .reduce((total, acumulator) => total + acumulator);
+
+    return { income, outcome, total: income - outcome };
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,67 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+
+// import { getCustomRepository } from 'typeorm';
+import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
+import ServiceCreate from './CreateTransactionService';
+// import ServiceJustValid from './JustValidTransaction';
+
+// import TransactionCustom from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  filename: string;
+}
+
+interface CSV {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
+  async execute({ filename }: RequestDTO): Promise<Transaction[]> {
     // TODO
+
+    // const repoTransaction = getCustomRepository(TransactionCustom);
+
+    const pathFileName = path.join(uploadConfig.directory, filename);
+
+    const readCSVStream = fs.createReadStream(pathFileName);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      // rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const lines: Transaction[] = [];
+
+    const service = new ServiceCreate();
+    parseCSV.on('data', async line => {
+      try {
+        const [title, type, value, category] = line;
+        const saved = await service.execute({ title, type, value, category });
+
+        lines.push(saved);
+      } finally {
+        console.log('opa');
+      }
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+      // parseCSV.on('error', err => {
+      //   throw new AppError(err.message, 400);
+      // });
+    });
+
+    return lines;
   }
 }
 
